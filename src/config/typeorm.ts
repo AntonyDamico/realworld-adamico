7e681670e1abba17036aed508c7ad61458af7d12
Antony D'Amico <antonydamico29@gmail.com>
2020-07-04 17:51:32 +0200

    Fixing linting errors

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,35 +1,19 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { createConnection } from 'typeorm';
 import dotenv from 'dotenv';
 import { join } from 'path';
-import { Client } from '../modules/client/ClientEntity';
 
 dotenv.config();
 
-// export const typeOrmConfg: PostgresConnectionOptions = {
-//     name: 'default',
-//     type: 'postgres',
-//     host: process.env.DATABASE_HOST,
-//     port: parseInt(process.env.DATABASE_PORT, 10),
-//     username: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_DB,
-//     synchronize: true,
-//     logging: true,
-//     // entities: [join(__dirname, `../modules/**/*Entity.{ts,js}`)],
-//     entities: [Client]
-// };
-
 createConnection({
-    name: 'default',
-    type: 'postgres',
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT, 10),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DB,
-    synchronize: true,
-    logging: true,
-    entities: [join(__dirname, `../modules/**/*Entity.{ts,js}`)],
-    // entities: [join(__dirname, `../**/*.js`)],
+  name: 'default',
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  synchronize: true,
+  logging: true,
+  entities: [join(__dirname, `../modules/**/*Entity.{ts,js}`)],
+  // entities: [join(__dirname, `../**/*.js`)],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import ClientRepository from './modules/client/ClientRepository';
 import express, { Request, Application, Response } from 'express';
 import dotenv from 'dotenv';
 // import { typeOrmConfg } from './config/typeorm';
@@ -10,29 +9,15 @@ import routes from './shared/routes';
 dotenv.config();
 const port = parseInt(process.env.SERVER_PORT, 10);
 
-// createConnection(typeOrmConfg)
-//     .then((connection) => {
 const app: Application = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ response: true });
+  res.status(200).json({ response: true });
 });
 
 app.use(routes);
 
-// app.get('/customerIndex', (req, res) => {
-//     const clientRepo = new ClientRepository();
-//     clientRepo.getClientsList().then((result) => res.send(result));
-// });
-
-// app.post('/customer', (req: Request, res) => {
-//     console.log(req.params);
-//     clientRepo.createClient(req.body).then((result) => res.send(result));
-// });
-
 app.listen(port, () => {
-    console.log(`Starting on port ${port}`);
+  console.log(`Starting on port ${port}`);
 });
-// })
-// .catch((error) => console.log(error));
