57432185a536d2bcc5eb4af416cef979efff630b
Antony D'Amico <antonydamico29@gmail.com>
2020-08-08 00:18:18 +0200

    Creating fake client repo create test

--- src/modules/client/__tests__/fakes/FakeClientRepository.test.ts
+++ src/modules/client/__tests__/fakes/FakeClientRepository.test.ts
@@ -8,11 +8,13 @@ describe('FakeClientRepository tests', () => {
     fakeClientRepository = new FakeClientRepository();
   });
 
-  it('should create a new client', async () => {
-    const newClient: IClientDTO = { name: 'test', lastname: 'client' };
-    const createdClient = await fakeClientRepository.createClient(newClient);
-    expect(createdClient).toHaveProperty('id');
-    // newClient.id = createdClient.id;
-    // expect(fakeClientRepository.clients).toContain(newClient);
+  describe('createClient', () => {
+    it('should create a new client', async () => {
+      const newClient: IClientDTO = { name: 'test', lastname: 'client' };
+      const createdClient = await fakeClientRepository.createClient(newClient);
+      expect(createdClient).toHaveProperty('id');
+      newClient.id = createdClient.id;
+      expect(fakeClientRepository.clients).toContainEqual(newClient);
+    });
   });
 });
