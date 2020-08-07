7155fb5fbe875a09411dcdc8d46b720411a5530c
Antony D'Amico <antonydamico29@gmail.com>
2020-08-08 00:52:54 +0200

    Creating get client and get list tests

--- src/modules/client/__tests__/fakes/FakeClientRepository.test.ts
+++ src/modules/client/__tests__/fakes/FakeClientRepository.test.ts
@@ -1,20 +1,38 @@
 import IClientDTO from '../../interfaces/IClientDTO';
 import FakeClientRepository from './FakeClientRepository';
+import Client from '../../ClientEntity';
 
 let fakeClientRepository: FakeClientRepository;
+let exampleClient: IClientDTO;
+let createdClient: Client;
 
 describe('FakeClientRepository tests', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     fakeClientRepository = new FakeClientRepository();
+    exampleClient = { name: 'test', lastname: 'client' };
+    createdClient = await fakeClientRepository.createClient(exampleClient);
   });
 
   describe('createClient', () => {
     it('should create a new client', async () => {
-      const newClient: IClientDTO = { name: 'test', lastname: 'client' };
-      const createdClient = await fakeClientRepository.createClient(newClient);
-      expect(createdClient).toHaveProperty('id');
-      newClient.id = createdClient.id;
-      expect(fakeClientRepository.clients).toContainEqual(newClient);
+      const localCreatedClient = await fakeClientRepository.createClient(exampleClient);
+      expect(localCreatedClient).toHaveProperty('id');
+      exampleClient.id = localCreatedClient.id;
+      expect(fakeClientRepository.clients).toContainEqual(exampleClient);
+    });
+  });
+
+  describe('getClientsList', () => {
+    it('should return a list with the', async () => {
+      const clientList = await fakeClientRepository.getClientsList();
+      expect(clientList).toEqual([createdClient]);
+    });
+  });
+
+  describe('getClient', () => {
+    it('should return the client data', async () => {
+      const clientData = await fakeClientRepository.getClient(createdClient.id);
+      expect(clientData).toEqual(createdClient);
     });
   });
 });
